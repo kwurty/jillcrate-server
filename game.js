3b957b7d317d7aef64cdae3ace2a62725a593444
kwurty <k.soroka@gmail.com>
2021-10-30 10:03:37 -0500

    move game settings to main index file for now

--- game.js
+++ game.js
@@ -3,31 +3,3 @@ module.exports = {
     generateGame
 }
 
-function generateGameSettings() {
-    return {
-        PLAYERS: [],
-        MAX_PLAYERS: 6,
-        ANSWER_TIMER: 10,
-        GAME_MODE: 'lives',
-        GAME_TYPES: ['lives', 'unlimited'],
-        MAX_LIVES: 3,
-        STATUS: 0,
-    }
-}
-
-function generateGame(gameSettings) {
-    let GAME = {
-        ...JSON.parse(gameSettings),
-        CURRENT_PLAYER: 0,
-        LAST_ANSWER_FIRSTNAME_LETTER: '',
-        LAST_ANSWER_LASTNAME_LETTER: '',
-        DIRECTION: 1
-    }
-
-    GAME.PLAYERS = GAME.PLAYERS.map((player) => ({
-        ...player,
-        LIVES: GAME.MAX_LIVES
-    }))
-
-    console.dir(GAME);
-}
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { instrument } = require('@socket.io/admin-ui');
 const { createServer } = require("http");
 const { createRoomCode } = require('./utilities');
-const { generateGameSettings, generateGame } = require('./game')
 
 const state = {};
 const socketRooms = {};
@@ -13,6 +12,66 @@ const io = require('socket.io')(httpServer, {
         origin: ['http://localhost:3000', "https://admin.socket.io"]
     }
 });
+function generateGameSettings() {
+    return {
+        PLAYERS: [],
+        MAX_PLAYERS: 6,
+        ANSWER_TIMER: 10,
+        GAME_MODE: 'lives',
+        GAME_TYPES: ['lives', 'unlimited'],
+        MAX_LIVES: 3,
+        STATUS: 0,
+    }
+}
+
+function generateGame(socket, gameSettings) {
+    let GAME = {
+        ...gameSettings,
+        CURRENT_PLAYER: 0,
+        LAST_ANSWER_LASTNAME_LETTER: '',
+        DIRECTION: 1,
+        TIME_LEFT: gameSettings.ANSWER_TIMER,
+        TIMER: setTimeout(() => {
+            this.TIME_LEFT--;
+            if (this.TIME_LEFT === 0) {
+                socket.emit('returnPlayerTurnTimeout');
+                this.TIME_LEFT = this.ANSWER_TIMER
+            }
+        }, 1000),
+        CHECK_ANSWER: function (answer) {
+            let s = answer.split(' ')
+            // check if the answer's first name starts with the letter of the last answer's last name
+            if (s[0][0].toLowerCase() === this.LAST_ANSWER_LASTNAME_LETTER) {
+
+                /// check if the last name also starts with the first letter of the last answer's last name
+                if (s[s.length - 1][0].toLowerCase() === this.LAST_ANSWER_LASTNAME_LETTER) {
+
+                }
+
+                /// no double name - set the last letter and move on to next person
+                else {
+
+                }
+
+            } else {
+                socket.emit('returnAnswer', false)
+            }
+        },
+        START_TIMER: function () {
+            this.TIMER()
+        },
+        GAME_OVER: function () {
+            socket.emit('returnGameWinner', this.PLAYERS[this.CURRENT_PLAYER]['name'])
+        }
+    }
+
+    GAME.PLAYERS = GAME.PLAYERS.map((player) => ({
+        ...player,
+        lives: GAME.MAX_LIVES
+    }))
+
+    console.dir(GAME);
+}
 
 function handleJoinGame(roomName) {
     const room = io.sockets.adapter.rooms[roomName];
@@ -51,10 +110,7 @@ function startGame(gameSettings) {
 
 
 io.on("connection", (socket) => {
-    console.log(socket.id);
-
     socket.on("joinRoom", (room, name) => {
-        // console.dir(io.sockets);
         try {
             // Check if the room exists
             const r = io.sockets.adapter.rooms;
